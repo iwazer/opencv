74f9ec60b73739e9d9983bc14b496562ddcb2eaa
Eiji Iwazawa <iwazawa@gmail.com>
2013-08-05 00:17:06 +0900

    cv::BORDER_* CONSTANTS in core/base.h are redefined as MCV::BORDER_*

---
+++ modules/motion/include/opencv2/motion/core_base.h
@@ -0,0 +1,16 @@
+/*
+ * from core/base.hpp
+ */
+
+enum {
+    MCV_BORDER_CONSTANT    = 0, // iiiiii|abcdefgh|iiiiiii  with some specified 'i'
+    MCV_BORDER_REPLICATE   = 1, // aaaaaa|abcdefgh|hhhhhhh
+    MCV_BORDER_REFLECT     = 2, // fedcba|abcdefgh|hgfedcb
+    MCV_BORDER_WRAP        = 3, // cdefgh|abcdefgh|abcdefg
+    MCV_BORDER_REFLECT_101 = 4, // gfedcb|abcdefgh|gfedcba
+    MCV_BORDER_TRANSPARENT = 5, // uvwxyz|absdefgh|ijklmno
+    
+    MCV_BORDER_REFLECT101  = MCV_BORDER_REFLECT_101,
+    MCV_BORDER_DEFAULT     = MCV_BORDER_REFLECT_101,
+    MCV_BORDER_ISOLATED    = 16 // do not look outside of ROI
+};
