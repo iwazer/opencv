48808797b9ae3997efe13df44e4bf35a8200fd97
Watson <watson1978@gmail.com>
2013-08-14 01:18:00 +0900

    delete trailing white spaces

--- modules/motion/src/MotionCV.mm
+++ modules/motion/src/MotionCV.mm
@@ -20,9 +20,9 @@
     CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
     CGFloat cols = image.size.width;
     CGFloat rows = image.size.height;
-    
+
     MotionMat *mat = [[MotionMat alloc] initWithRows:rows cols:cols channels:CV_8UC4];
-    
+
     CGContextRef contextRef = CGBitmapContextCreate([mat data],                 // Pointer to  data
                                                     cols,                       // Width of bitmap
                                                     rows,                       // Height of bitmap
@@ -31,11 +31,11 @@
                                                     colorSpace,                 // Colorspace
                                                     kCGImageAlphaNoneSkipLast |
                                                     kCGBitmapByteOrderDefault); // Bitmap info flags
-    
+
     CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
     CGContextRelease(contextRef);
     CGColorSpaceRelease(colorSpace);
-    
+
     return mat;
 }
 
@@ -43,15 +43,15 @@
 {
     NSData *data = [NSData dataWithBytes:[mat data] length:[mat elemSize]*[mat total]];
     CGColorSpaceRef colorSpace;
-    
+
     if ([mat elemSize] == 1) {
         colorSpace = CGColorSpaceCreateDeviceGray();
     } else {
         colorSpace = CGColorSpaceCreateDeviceRGB();
     }
-    
+
     CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)data);
-    
+
     // Creating CGImage from cv::Mat
     CGImageRef imageRef = CGImageCreate([mat cols],                                 //width
                                         [mat rows],                                 //height
@@ -65,13 +65,13 @@
                                         false,                                      //should interpolate
                                         kCGRenderingIntentDefault                   //intent
                                         );
-    
+
     // Getting UIImage from CGImage
     UIImage *finalImage = [UIImage imageWithCGImage:imageRef];
     CGImageRelease(imageRef);
     CGDataProviderRelease(provider);
     CGColorSpaceRelease(colorSpace);
-    
+
     return finalImage;
 }
 
